4d3816cf17a35dcaf478b4fee9bda25d4e1a1d9c
c02vm1 <c02vm1@192.168.254.1>
2018-12-19 19:53:55 +0000

    Second cut additional policies - all compile NSA tested ok

---
+++ service_model/tbsm_additional_policies/TBSM_cp_push_policies.sh
@@ -0,0 +1,7 @@
+$3/IBM/tivoli/impact/bin/nci_policy TBSM push $1 $2 TBSM_cp_enricher.ipl --project TBSM
+$3/IBM/tivoli/impact/bin/nci_policy TBSM push $1 $2 TBSM_cp_publisher.ipl --project TBSM
+$3/IBM/tivoli/impact/bin/nci_policy TBSM push $1 $2 TBSM_cp_nsa_generate.ipl --project TBSM
+$3/IBM/tivoli/impact/bin/nci_policy TBSM push $1 $2 TBSM_cp_nsa_update.ipl --project TBSM
+$3/IBM/tivoli/impact/bin/nci_policy TBSM push $1 $2 TBSM_cp_statustext.ipl --project TBSM
+$3/IBM/tivoli/impact/bin/nci_policy TBSM push $1 $2 TBSM_cp_utils.ipl --project TBSM
+$3/IBM/tivoli/impact/bin/nci_policy TBSM push $1 $2 TBSM_cp_l3_overall.ipl --project TBSM
